f19c053515fb9b4b7fb54c17ab026976d1987da8
MarcioJr <91279020+uMarcinho@users.noreply.github.com>
2025-09-09 10:19:25 -0300

    Delete src directory
    remove code black hole project

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            BlackHoleApp app = new BlackHoleApp();
-            app.show();
-        });
-    }
-}
